1ffcce8054dc6487504098f0abba6a705ee4ce89
Wattanit Hotrakool <wattanit.h@gmail.com>
2019-08-30 01:18:42 +0700

    Add files for PyPI distribution

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'deprint',
+  packages = ['deprint'],
+  version = '0.1',
+  license='MIT',
+  description = 'Declarative Python fancy print statement',
+  author = 'Wattanit Hotrakool',
+  author_email = 'wattanit@protonmail.com',
+  url = 'https://github.com/rorasa/deprint',
+  download_url = 'https://github.com/rorasa/deprint/archive/v_01.tar.gz',
+  keywords = ['declarative', 'print', 'fancy', 'color'],
+#   install_requires=[ 
+#       ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
